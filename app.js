d841699ddcc6b0ebd31c4af74cf3bb130f99d325
Shahzad-Akram <shahzadakram2244@gmail.com>
2020-07-13 22:56:51 +0500

    changes

--- app.js
+++ app.js
@@ -7,54 +7,54 @@ app.use(express.json());
 app.use(cors());
 
 const SendEmail = (text, from, subject, to) => {
-  return new Promise((resolve, reject) => {
-    const transporter = nodemailer.createTransport({
-      host: 'smtp.gmail.com.',
-      secure: false,
-      port: 587,
-      auth: {
-        user: 'davidleando18@gmail.com',
-        pass: 'Gotohell123',
-      },
-    });
-    const mailOptions = {
-      from,
-      to,
-      subject,
-      html: `<h1>From: ${from}</h1>
+	return new Promise((resolve, reject) => {
+		const transporter = nodemailer.createTransport({
+			host: 'smtp.gmail.com.',
+			secure: false,
+			port: 587,
+			auth: {
+				user: 'servermailingservice@gmail.com',
+				pass: 'mailservice'
+			}
+		});
+		const mailOptions = {
+			from,
+			to,
+			subject,
+			html: `<h1>From: ${from}</h1>
         <p>${text}</p>
-      `,
-    };
+      `
+		};
 
-    transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-        reject();
-      } else {
-        resolve();
-      }
-    });
-  });
+		transporter.sendMail(mailOptions, (error, info) => {
+			if (error) {
+				reject();
+			} else {
+				resolve();
+			}
+		});
+	});
 };
 
 app.post('/', (req, res) => {
-  const { text, from, subject, to } = req.body;
-  SendEmail(text, from, subject, to)
-    .then((sent) => {
-      res.json({
-        Message: 'Email Sent!',
-        status: false,
-      });
-    })
-    .catch((err) => {
-      res.json({
-        Message: 'Email Not Sent!',
-        status: false,
-      });
-    });
+	const { text, from, subject, to } = req.body;
+	SendEmail(text, from, subject, to)
+		.then((sent) => {
+			res.json({
+				Message: 'Email Sent!',
+				status: 200
+			});
+		})
+		.catch((err) => {
+			res.json({
+				Message: 'Email Not Sent!',
+				status: false
+			});
+		});
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Listening on Port ${PORT}`);
+	console.log(`Listening on Port ${PORT}`);
 });
